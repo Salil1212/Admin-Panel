3d873475c6878fa330a173328fa67e0ff5689317
Salil Nigam <114912638+Salil1212@users.noreply.github.com>
2024-10-31 13:36:24 +0000

    addingValidation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { register, login, logout } = require("../controllers/authController");
-// const {register1} = require("../validations/auth.validation");
-// const { validate } = require("express-validation");
+const  controller  = require("../controllers/authController");
+const {register, login} = require("../validations/auth.validation");
+const { validate } = require("express-validation");
 const router = express.Router();
 
-router.post("/register", register);
-// router.route("/register").post(validate(register1),register);
-router.post("/login", login);
-router.post("/logout", logout);
+router.route("/register").post(validate(register),controller.register);
+router.route("/login").post(validate(login),controller.login);
+router.route("/logout").post( controller.logout);
 
 module.exports = router;
--- server/validations/auth.validation.js
+++ server/validations/auth.validation.js
@@ -1,14 +1,22 @@
 const { Joi } = require('express-validation');
 
 module.exports = {
-    register1: {
+    register: {
         body:Joi.object(
             {
                 firstName: Joi.string().required(), 
                 lastName: Joi.string().required(), 
-                email: Joi.string().required(),
-                password: Joi.string().required(), 
-                role: Joi.string().required()
+                email: Joi.string(),
+                password: Joi.string().required().min(4).max(128),
+                role: Joi.string().valid("User", "Admin"),
+            }
+        )
+    },
+    login: {
+        body: Joi.object(
+            {
+                email: Joi.string(),
+                password: Joi.string().required().min(4).max(128),
             }
         )
     }
